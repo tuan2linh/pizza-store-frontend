e693a249cac5b64dc0ae555c3e5b4b967858f0ba
tuan2linh <champihycoql@gmail.com>
2024-12-07 16:31:36 +0700

    Update axiosCustomize.js

--- src/utils/axiosCustomize.js
+++ src/utils/axiosCustomize.js
@@ -3,7 +3,7 @@ import {store} from '../redux/store';
 
 
 const instance = axios.create({
-    baseURL: 'http://54.153.176.43/api',
+    baseURL: 'http://localhost:8081/api',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
